ce05a0b563faaebe42825d678bd5641ee699dbf8
Abbey Kato <amk7306@psu.edu>
2020-09-08 23:48:35 +0000

    getlettergrade

--- main.py
+++ main.py
@@ -5,30 +5,30 @@
 # Section: 1
 # Breakout: 12
 
-import main
+def run():
+  grade = float(input("Enter your CMPSC 131 grade: "))
+  g = getLetterGrade(grade) 
+  print(f"Your letter grade for CMPSC 131 is {g}.")
 def getLetterGrade(grade):
-  grade = float(grade)
-  #grade = input("Enter your CMPSC 131 grade: ")
   if grade == 100:
     return "A+"
-  if 93<=grade and grade<100: 
+  elif 93<=grade and grade<100: 
     return "A"
-  if 90<=grade and grade<93:
+  elif 90<=grade and grade<93:
     return "A-"
-  if 87<=grade and grade<90:
+  elif 87<=grade and grade<90:
     return "B+"
-  if 83<=grade and grade<87:
+  elif 83<=grade and grade<87:
     return "B"
-  if 80<=grade and grade<83:
+  elif 80<=grade and grade<83:
     return "B-"
-  if 76<=grade and grade<80:
+  elif 76<=grade and grade<80:
     return "C+"
-  if 70<=grade and grade<76:
+  elif 70<=grade and grade<76:
     return "C"
-  if 60<=grade and grade<70:
+  elif 60<=grade and grade<70:
     return "D"
-  if  60 > grade: 
+  else: 
     return "F"
-  #print(f"Your letter grade for CMPSC 131 is: {grade}")
 if __name__ == "__main__":
-  getLetterGrade()
+  run()
