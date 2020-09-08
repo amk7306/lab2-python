ed4e42b5f803cb7fc88e9df66c6db7658f73002b
Abbey Kato <amk7306@psu.edu>
2020-09-08 23:51:47 +0000

    a

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ def run():
   g = getLetterGrade(grade) 
   print(f"Your letter grade for CMPSC 131 is {g}.")
 def getLetterGrade(grade):
-  if grade == 100:
-    return "A+"
-  elif 93<=grade and grade<100: 
+  if 93<=grade and grade>100: 
     return "A"
   elif 90<=grade and grade<93:
     return "A-"
